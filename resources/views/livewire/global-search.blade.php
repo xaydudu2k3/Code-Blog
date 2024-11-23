faff3183a3144415e30b3235503495cadb5fb3fa
Vinh_Nguyen <128900315+EkkoNguyen@users.noreply.github.com>
2024-11-23 09:57:10 +0700

    global search for admin page

--- resources/views/livewire/global-search.blade.php
+++ resources/views/livewire/global-search.blade.php
@@ -1,21 +1,21 @@
 <div class="dropdown-wrapper" style="position: relative;">
     <input type="text" wire:model.live="search" placeholder="Search" class="form-control" />
     @if ($results)
-        <div class="dropdown-menu d-block mt-3"
-            style="position: absolute; top: 100%; left: 0; right: 0; min-width: 400px;">
-            @foreach ($results as $post)
-                <a class="dropdown-item" href="/view/post/{{ $post->id }}" wire:navigate
-                    wire:click="addViewers({{ $post->id }})">
-                    <img src="{{ asset('storage/images/' . $post->photo) }}" width="50px" height="50px"
-                        alt="Post Image" class="post-img">
-                    <strong>{{ $post->post_title }}</strong> by {{ $post->user->name }}
-                    <br>
-                    Tags:
-                    @foreach ($post->tags as $tag)
-                        <span class="badge bg-secondary">{{ $tag->name }}</span>
-                    @endforeach
-                </a>
+    <div class="dropdown-menu d-block mt-3"
+        style="position: absolute; top: 100%; left: 0; right: 0; min-width: 400px;">
+        @foreach ($results as $post)
+        <a class="dropdown-item" href="/view/post/{{ $post->id }}"
+            wire:click="addViewers({{ $post->id }})">
+            <img src="{{ asset('storage/images/' . $post->photo) }}" width="50px" height="50px"
+                alt="Post Image" class="post-img">
+            <strong>{{ $post->post_title }}</strong> by {{ $post->user->name }}
+            <br>
+            Tags:
+            @foreach ($post->tags as $tag)
+            <span class="badge bg-secondary">{{ $tag->name }}</span>
             @endforeach
-        </div>
+        </a>
+        @endforeach
+    </div>
     @endif
 </div>
